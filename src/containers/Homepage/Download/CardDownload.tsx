51a1e51e7f200bd93b7ef9b38820bb0ea33c9bbe
Faisal Amir <urmauur@gmail.com>
2024-07-05 23:00:09 +0700

    fix: update label linux package

--- src/containers/Homepage/Download/CardDownload.tsx
+++ src/containers/Homepage/Download/CardDownload.tsx
@@ -37,7 +37,7 @@ const systemsTemplate: SystemType[] = [
   },
   {
     name: "Linux",
-    label: "AppImage",
+    label: "Deb",
     logo: FaLinux,
     fileFormat: "{appname}-installer-{tag}-amd64-linux.deb",
   },
